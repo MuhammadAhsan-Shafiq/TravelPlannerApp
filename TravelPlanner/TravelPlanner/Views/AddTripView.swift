7530c3ffabc62d629f3e50fef80b7f670fa9d102
Muhammad Ahsan <ma5639176@gmail.com>
2024-08-15 18:06:44 +0500

    Enhanced UI and added trip creation feature

---
+++ TravelPlanner/TravelPlanner/Views/AddTripView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTripView.swift
+//  TravelPlanner
+//
+//  Created by MacBook Pro on 15/08/2024.
+//
+
+import SwiftUI
+
+struct AddTripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTripView()
+}
