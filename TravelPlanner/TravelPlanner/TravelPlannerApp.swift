34ee039fe7759831e6386e2d025d94c9af917f38
Muhammad Ahsan <ma5639176@gmail.com>
2024-08-15 16:08:47 +0500

    Initial commit with Xcode project setup for TravelPlannerApp

---
+++ TravelPlanner/TravelPlanner/TravelPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlannerApp.swift
+//  TravelPlanner
+//
+//  Created by MacBook Pro on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
